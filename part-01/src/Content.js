5326c515dad5650be46fa43f0e1aef91c1d3d4a4
jordanhailey <jordan.m.hailey@gmail.com>
2021-09-02 12:45:30 -0700

    init Content component and tests
    Init Content Component
    
    init Content component tests
    
    init Content component
    
    Test failing for 'part' and 'exercises' strings
    
    Passing test for 'part' and 'exercises' strings
    
    new expectation, failing to read props
    
    Content component - only renders if supplied correctly typed props

---
+++ part-01/src/Content.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Content = (props) => {
+  const { part, numExercises } = props;
+  return (!part || typeof part == "object") ? null : (
+    <div>
+      <div className="part">
+        <span>Part:</span>
+        <span>{part}</span>
+      </div>
+      <div className="exercises">
+        <span>Exercises:</span>
+        <span>{Number(numExercises) == Number(numExercises) ? numExercises : 0}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Content
---
+++ part-01/src/Content.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Content from './Content';
+
+const testPart = "Part 01";
+const testPartNumExercises = 10;
+
+test('Component only renders if the "part" prop is passed in.', () => {
+  const content = render(<Content />);
+  expect(()=>{screen.getAllByText(/./)}).toThrow();
+  content.rerender(<Content part={testPart} />);
+  expect(screen.getAllByText(/./).length > 0);
+});
+
+test('The "part" prop must be non-object value.', () => {
+  const content = render(<Content part={{part:testPart}} />);
+  expect(()=>{screen.getAllByText(/./)}).toThrow();
+  content.rerender(<Content part={testPart} />);
+  expect(screen.getByText(new RegExp(testPart,"i"))).toBeInTheDocument();
+});
+
+test('The "numExercises" prop must be able to be coerced to a number.', () => {
+  const content = render(<Content part={testPart} numExercises={testPartNumExercises} />);
+  expect(screen.getByText(/part:/i)).toBeInTheDocument();
+  expect(screen.getByText(/exercises:/i)).toBeInTheDocument();
+  expect(screen.getByText(new RegExp(testPart,"i"))).toBeInTheDocument();
+  expect(screen.getByText(new RegExp(testPartNumExercises,"i"))).toBeInTheDocument();
+});
